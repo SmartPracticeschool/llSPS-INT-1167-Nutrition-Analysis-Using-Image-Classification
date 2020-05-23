4c6f844d70e7e5254cd1937f04919592f472e1c7
jayanth-9 <65609741+jayanth-9@users.noreply.github.com>
2020-05-23 13:16:54 +0530

    Add files via upload

---
+++ flask/hello.py
@@ -0,0 +1,52 @@
+from __future__ import division, print_function
+from flask import Flask, redirect, url_for, request, render_template, jsonify
+import json
+from werkzeug.utils import secure_filename
+from keras.models import load_model
+from keras.preprocessing import image
+import numpy as np
+import sys
+import os
+
+
+app = Flask(__name__, static_url_path='')
+
+def model_predict(img_path, model):
+    img = image.load_img(img_path, target_size=(64, 64))
+    x = image.img_to_array(img)
+    x = np.expand_dims(x, axis=0)
+    preds = model.predict_classes(x)
+    return preds
+
+
+@app.route('/', methods=['GET'])
+def index():
+    return render_template('index.html')
+
+
+@app.route('/predict', methods=['GET', 'POST'])
+def upload():
+    if request.method == 'POST':
+        f = request.files['image']
+
+        basepath = os.path.dirname(__file__)
+        file_path = os.path.join(
+            basepath, 'uploads', secure_filename(f.filename))
+        f.save(r'C:\Users\jayan\Desktop\Internship project1\Cnn.h5')
+        model = load_model("Cnn.h5")
+        preds = model_predict(file_path, model)
+        print("preds : "+str(preds))
+        ls=["Banana","Guava","Mango","Orange"]
+        p=preds.flatten()
+        result = ls[p[0]]
+        result = ls[preds[0]]             
+        return result
+    return None
+		
+
+if __name__ == '__main__':
+    port = int(os.getenv('PORT', 8050))
+    app.run(host='0.0.0.0', port=port, debug=True)
+
+
+
